1b2c38b6138f4217bf2cfa94e6de5b3357ae5463
Huan Oyosa <jarodriguezo87@gmail.com>
2015-12-07 20:33:10 -0600

    Adds docstrings and basic missing methods

--- app/src/main/java/org/kira/kiralist/KiraList.java
+++ app/src/main/java/org/kira/kiralist/KiraList.java
@@ -14,13 +14,35 @@ public class KiraList {
         shopCar = new ShopCar();
     }
 
+    /**
+     * Add item to wishlist
+     * @param item item to add
+     * @return ArrayList<String>
+     */
     public ArrayList<String> addItem(String item){
         wishList.add(item);
         return wishList;
     }
 
+    /**
+     * Returns the wishlist for the object
+     * @return ArrayList<String>
+     */
     public ArrayList<String> getWishList(){
         return wishList;
+    }
+
+    /**
+     * Adds the specified item to the shop car and deletes it from the wish list
+     * @param item_index wishlist item to add
+     * @param price price for the item
+     * @param quantity quantity of items
+     * @return ShopCar
+     */
+    public ShopCar addItemToCar(int item_index, float price, float quantity){
+        shopCar.addToCar(wishList.get(item_index), price, quantity);
+        wishList.remove(item_index);
+        return shopCar;
     }
 
     class ShopCar{
@@ -30,11 +52,22 @@ public class KiraList {
             items = new ArrayList<>();
         }
 
+        /**
+         * Adds an item to the shopping car
+         * @param item item to add
+         * @param price price of the item
+         * @param quantity quantity of items
+         * @return ArrayList<CarItem>
+         */
         public ArrayList<CarItem> addToCar(String item, float price, float quantity){
             items.add(new CarItem(item, price, quantity));
             return items;
         }
 
+        /**
+         * Computes the total amount for the shopping
+         * @return Float
+         */
         public Float getGrandTotal(){
             float total = 0;
             for (CarItem item : items){
@@ -51,6 +84,54 @@ public class KiraList {
             public CarItem(String item, float price, float quantity){
                 this.item = item;
                 this.price = price;
+                this.quantity = quantity;
+            }
+
+            /**
+             * Returns the Item
+             * @return String
+             */
+            public String getItem(){
+                return item;
+            }
+
+            /**
+             * Sets the value for the item's name
+             * @param item the name of the item
+             */
+            public void setItem(String item){
+                this.item = item;
+            }
+
+            /**
+             * Returns the price of the item
+             * @return float
+             */
+            public float getPrice(){
+                return price;
+            }
+
+            /**
+             * Sets the value for the item's price
+             * @param price the price of the item
+             */
+            public void setPrice(float price){
+                this.price = price;
+            }
+
+            /**
+             * Returns the quantity of items to buy
+             * @return float
+             */
+            public float getQuantity(){
+                return quantity;
+            }
+
+            /**
+             * Sets the quantity of items to buy
+             * @param quantity
+             */
+            public void setQuantity(float quantity){
                 this.quantity = quantity;
             }
         }
