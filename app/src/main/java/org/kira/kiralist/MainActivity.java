793e47d9812e461e34c79f68c58d315ff54a98cd
Antonio de Mora <antonio.dmora@gmail.com>
2015-12-08 16:54:01 -0600

    Adds the addItem method requested by the add button
    Now, we can show the user the item added to the wish list

--- app/src/main/java/org/kira/kiralist/MainActivity.java
+++ app/src/main/java/org/kira/kiralist/MainActivity.java
@@ -2,13 +2,16 @@ package org.kira.kiralist;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
-import android.widget.TableLayout;
+import android.widget.ListView;
 
 public class MainActivity extends Activity {
     private EditText et_item;
-    private TableLayout tl_items;
+    private ListView lst_items;
     private KiraList kiraList;
+    private ArrayAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +19,20 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         kiraList = new KiraList();
         et_item = (EditText) findViewById(R.id.et_item);
-        tl_items = (TableLayout) findViewById(R.id.tl_items);
+        lst_items = (ListView) findViewById(R.id.lst_items);
+
+        adapter = new ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_list_item_1,
+                kiraList.getWishList()
+        );
+
+        lst_items.setAdapter(adapter);
+    }
+
+    public void addItem(View view){
+        String item = et_item.getText().toString();
+        et_item.setText("");
+        adapter.insert(item, 0);
     }
 }
