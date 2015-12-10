8a0fe1375982a97e82386db6c5ea8fae3bd44e7b
Huan Oyosa <jarodriguezo87@gmail.com>
2015-12-10 15:01:59 -0600

    Adds dialogs on wishlist
    Now we prompt the user for price and quantity on adding an item
    and ask him if sure about deleting an item when longclicks it

--- app/src/main/java/org/kira/kiralist/MainActivity.java
+++ app/src/main/java/org/kira/kiralist/MainActivity.java
@@ -5,14 +5,13 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.ListView;
-import android.widget.TextView;
 
 import org.kira.kiralist.core.KiraList;
 
@@ -46,8 +45,14 @@ public class MainActivity extends Activity {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 showDialogAddToCart(position);
-//                kiraList.addItemToCar(position, 5, 1);
-//                adapter.notifyDataSetChanged();
+            }
+        });
+
+        wishlist.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                showDialogDeleteItem(position);
+                return true;
             }
         });
     }
@@ -73,14 +78,22 @@ public class MainActivity extends Activity {
     public void showDialogAddToCart(final int position){
         String item = kiraList.getWishList().get(position);
         AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
-        dialogBuilder.setView(getLayoutInflater().inflate(R.layout.dialog_add_to_car, null));
         dialogBuilder.setMessage(item);
+        final EditText txt_price = new EditText(this);
+        txt_price.setInputType(EditorInfo.TYPE_CLASS_NUMBER | EditorInfo.TYPE_NUMBER_FLAG_DECIMAL);
+        txt_price.setHint(R.string.label_price);
+        final EditText txt_quantity = new EditText(this);
+        txt_quantity.setInputType(EditorInfo.TYPE_CLASS_NUMBER | EditorInfo.TYPE_NUMBER_FLAG_DECIMAL);
+        txt_quantity.setHint(R.string.label_quantity);
+        LinearLayout dialog_view = new LinearLayout(this);
+        dialog_view.setOrientation(LinearLayout.VERTICAL);
+        dialog_view.addView(txt_price);
+        dialog_view.addView(txt_quantity);
+        dialogBuilder.setView(dialog_view);
 
         dialogBuilder.setPositiveButton(R.string.accept, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-                EditText txt_price = (EditText) findViewById(R.id.txt_price);
-                EditText txt_quantity = (EditText) findViewById(R.id.txt_quantity);
                 float price = Float.parseFloat(txt_price.getText().toString());
                 float quantity = Float.parseFloat(txt_quantity.getText().toString());
                 kiraList.addItemToCar(position, price, quantity);
@@ -96,6 +109,30 @@ public class MainActivity extends Activity {
             }
         });
 
+        AlertDialog dialog = dialogBuilder.create();
+        dialog.show();
+    }
+
+    public void showDialogDeleteItem(final int position){
+        String item = kiraList.getWishList().get(position);
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
+        dialogBuilder.setMessage(getString(R.string.delete_item) + item + '?');
+
+        dialogBuilder.setPositiveButton(R.string.accept, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                kiraList.deleteItem(position);
+                adapter.notifyDataSetChanged();
+                dialog.dismiss();
+            }
+        });
+
+        dialogBuilder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
 
         AlertDialog dialog = dialogBuilder.create();
         dialog.show();
